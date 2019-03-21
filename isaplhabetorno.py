4dc288c9eb80067f67c0eb923b7f3407bfdfd4fc
amritavarshi <amritavarshini12@gmail.com>
2019-03-21 17:12:12 +0530

    Add files via upload

---
+++ isaplhabetorno.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 21 17:03:01 2019
+
+@author: hp
+"""
+
+ch=input()
+if ((ch>='a') and (ch<='z') )or ((ch>='A') and (ch<='Z')):
+    print ("Alphabet")
+else:
+    print ("No")
