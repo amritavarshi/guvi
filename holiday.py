19da25bcc6c494f32225b3c157174de2b5bd4618
amritavarshi <amritavarshini12@gmail.com>
2019-04-05 23:37:55 +0530

    Add files via upload

---
+++ holiday.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  5 23:35:17 2019
+
+@author: hp
+"""
+
+s=input()
+if (s=="saturday") or (s=="Saturday") or (s=="Sunday") or (s=="sunday"):
+    print("yes")
+else:
+    print("no")
