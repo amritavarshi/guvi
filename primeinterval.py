f92bd11130c40d9bb6d7194efcb4db30d513ae74
amritavarshi <amritavarshini12@gmail.com>
2019-04-05 23:27:30 +0530

    Add files via upload

--- primeinterval.py
+++ primeinterval.py
@@ -8,14 +8,13 @@ Created on Fri Apr  5 22:28:55 2019
 l,u=input().split()
 l=int(l)
 u=int(u)
-for num in range(l+1,u+1):
+for num in range(l+1,u):
    # prime numbers are greater than 1
    if num > 1:
        for i in range(2,num):
            if (num % i) == 0:
                break
        else:
-           print(num)
-        
+           print(num)   
             
         
