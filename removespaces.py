0f625e98f576cae2cc41334edc125b8e5c79fe1c
Amrita Varshini <amritavarshini12@gmail.com>
2019-05-24 02:46:18 +0530

    Add files via upload

---
+++ removespaces.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 24 02:43:08 2019
+
+@author: hp
+"""
+
+def remove(string):
+    return string.replace(" ","")
+string=input()
+print(remove(string))
