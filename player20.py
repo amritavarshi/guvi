e7c47405015cf0e1f0bd292abafc576ab697ae06
Amrita Varshini <amritavarshini12@gmail.com>
2019-07-10 20:56:06 +0530

    Delete player20.py

--- player20.py
+++
@@ -1,9 +0,0 @@
-string=input()
-string=list(string)
-encrypted=[]
-for i in range(len(string)):
-    if (string[i]=='X'or'Y'or'Z'):
-        encrypted.append(chr(ord(string[i])-23))
-    else:
-        encrypted.append(chr(ord(string[i])+3))
-print(*encrypted,sep="")
