967287a0f9adb9cac4811f26f2175c4ae5504a23
Amrita Varshini <amritavarshini12@gmail.com>
2019-07-11 10:17:51 +0530

    Update pro12.py

--- pro12.py
+++ pro12.py
@@ -3,5 +3,5 @@ arr=list(map(int,input().split()))
 asum=[]
 for i in range(t):
     u,v=map(int,input().split())
-    asum.append(sum(arr[u:v+1]))
+    asum.append(sum(arr[u-1:v]))
 print(*asum,sep="\n")
