1254c125bf08c71ce0bf8f3d7558d46056116bdd
Amrita Varshini <amritavarshini12@gmail.com>
2019-07-11 20:21:00 +0530

    Add files via upload

---
+++ pro52.py
@@ -0,0 +1,8 @@
+x1,y1=map(int,input().split())
+x2,y2=map(int,input().split())
+x3,y3=map(int,input().split())
+x4,y4=map(int,input().split())
+if(abs(x1-x2)==(x4-x3))==abs((y1-y4)==(y2-y3)):
+    print("yes")
+else:
+    print("no")
